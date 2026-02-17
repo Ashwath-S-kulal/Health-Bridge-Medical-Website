0d194aca7fc98fee38bfc6160a9ee3c427babef5
Ashwath-S-kulal <ashwathkulal2004@gmail.com>
2026-02-17 09:49:59 +0530

    added

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -29,7 +29,7 @@ export default function Profile() {
     e.preventDefault();
     try {
       dispatch(updateUserStart());
-      const res = await fetch(`/api/user/update/${currentUser._id}`, {
+      const res = await fetch(`${import.meta.env.VITE_BASE_URI}/api/user/update/${currentUser._id}`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
