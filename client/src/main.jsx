e482d9459f0d5003c49eb8196ca89d986826490c
Ashwath-S-kulal <ashwathkulal2004@gmail.com>
2026-02-22 13:07:55 +0530

    f

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -7,12 +7,6 @@ import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 
 
-import { registerSW } from 'virtual:pwa-register'
-
-registerSW({ immediate: true })
-
-
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
